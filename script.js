ecb486ee0751ab0ebd177037587855aed144dc3f
mollah111 <jalammollah111@gmail.com>
2024-10-17 23:38:15 +0600

    Array

---
+++ script.js
@@ -0,0 +1,78 @@
+// 1D Array (Index Array)
+
+let indexArray = [89, 56, 78, 37, 29, 64, 45, 95];
+
+console.log(indexArray[6]);
+console.log(indexArray[7]);
+console.log(indexArray[4]);
+console.log(indexArray[1]);
+console.log(indexArray[5]);
+console.log(indexArray[3]);
+console.log(indexArray[2]);
+console.log(indexArray[0]);
+
+// Associative Array (Objective)
+
+let student = {name: "Monem Sharhriar", phone: +8801300777110, email: "monem1998@gmail.com", address: "Shibpur, Narsingdi", age: 26};
+
+console.log(student.name);
+console.log(student.phone);
+console.log(student.email);
+console.log(student.address);
+console.log(student.age);
+
+// 2D Array
+
+let twoDimensionalArray = [
+    [11, 21, 31, 41, 51, 61, 71, 81, 91],
+    [12, 22, 32, 42, 52, 62, 72, 82, 92],
+    [13, 23, 33, 43, 53, 63, 73, 83, 93],
+    [14, 24, 34, 44, 54, 64, 74, 84, 94],
+    [15, 25, 35, 45, 55, 65, 75, 85, 95],
+    [16, 26, 36, 46, 56, 66, 76, 86, 96],
+    [17, 27, 37, 47, 57, 67, 77, 87, 97],
+    [18, 28, 38, 48, 58, 68, 78, 88, 98],
+    [19, 29, 39, 49, 59, 69, 79, 89, 99],
+    [20, 30, 40, 50, 60, 70, 80, 90, 100],
+]
+
+console.log(twoDimensionalArray[0][0]);
+console.log(twoDimensionalArray[0][1]);
+console.log(twoDimensionalArray[0][2]);
+console.log(twoDimensionalArray[0][3]);
+console.log(twoDimensionalArray[0][4]);
+console.log(twoDimensionalArray[0][5]);
+console.log(twoDimensionalArray[0][6]);
+console.log(twoDimensionalArray[0][7]);
+console.log(twoDimensionalArray[0][8]);
+
+console.log(twoDimensionalArray[1][0]);
+console.log(twoDimensionalArray[1][1]);
+console.log(twoDimensionalArray[1][2]);
+console.log(twoDimensionalArray[1][3]);
+console.log(twoDimensionalArray[1][4]);
+console.log(twoDimensionalArray[1][5]);
+console.log(twoDimensionalArray[1][6]);
+console.log(twoDimensionalArray[1][7]);
+console.log(twoDimensionalArray[1][8]);
+
+console.log(twoDimensionalArray[2][0]);
+console.log(twoDimensionalArray[2][1]);
+console.log(twoDimensionalArray[2][2]);
+console.log(twoDimensionalArray[2][3]);
+console.log(twoDimensionalArray[2][4]);
+console.log(twoDimensionalArray[2][5]);
+console.log(twoDimensionalArray[2][6]);
+console.log(twoDimensionalArray[2][7]);
+console.log(twoDimensionalArray[2][8]);
+
+console.log(twoDimensionalArray[3][0]);
+console.log(twoDimensionalArray[3][1]);
+console.log(twoDimensionalArray[3][2]);
+console.log(twoDimensionalArray[3][3]);
+console.log(twoDimensionalArray[3][4]);
+console.log(twoDimensionalArray[3][5]);
+console.log(twoDimensionalArray[3][6]);
+console.log(twoDimensionalArray[3][7]);
+console.log(twoDimensionalArray[3][8]);
+
